700b2ba58d4d270bcf5906e533f31dd77b6d62c2
KrithigaP <86260992+KrithigaParamasivam@users.noreply.github.com>
2021-07-24 14:02:17 +0530

    Add files via upload

---
+++ springbootWK01B2/Multimodelmaven/string/src/main/java/com/pack/StringFunctions.java
@@ -0,0 +1,19 @@
+package com.pack;
+
+public class StringFunctions {
+	public static boolean display(String s1,String s2) {
+		return s1.equalsIgnoreCase(s2);
+		
+	}	
+	public static int display1(String s1,String s2) {
+		return s1.length();
+		
+		
+	}	
+	public static String display2(String s1) {
+		return s1.toUpperCase();
+		
+		
+	}	
+	}
+
